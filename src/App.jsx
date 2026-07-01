b6818b514d36453ab78ae3fe8f1148b8663ed9c4
JustRizky02 <rizky30054@gmail.com>
2026-07-01 22:51:42 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './index.css'
+
+function App() {
+  return (
+      <div className="min-h-screen bg-blue-500 flex items-center justify-center">
+        <h1 className="text-white text-5xl font-bold">
+          Tailwind Berhasil!
+        </h1>
+      </div>
+  )
+}
+
+export default App
